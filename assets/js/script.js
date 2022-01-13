17cdba27f7f4d5c248e1b2f06bff89207f47fdd0
Giovanni Barranco <giovannibarranco@Giovannis-iMac.lan>
2022-01-13 12:25:47 -0700

    added for loop & tested api links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Marco's Code
+// // Marco's Code
 var apiUrlNews = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=covid-cases&api-key=vULfp6mXKDYYuz3IoryCuILRxwbMZh4O";
 
 fetch(apiUrlNews)
@@ -21,7 +21,9 @@ fetch(apiUrlLocation)
   .then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
-        console.log(data);
+        for(i=0; i < data.length; i++){
+          console.log(data[i].physical_address[0].city);
+        }
       });
     } else {
       alert('Error: ' + response.statusText);
@@ -30,6 +32,8 @@ fetch(apiUrlLocation)
   .catch(function (error) {
     alert('Error');
   });
+
+
 
 var apiUrlCovidActNow = "https://api.covidactnow.org/v2/state/CA.json?apiKey=e8eb26806f83418eade3b431bfc5fc2c";
 
@@ -47,72 +51,30 @@ fetch(apiUrlCovidActNow)
     alert('Error');
   });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // End of Marco's Code
 // Gio's Code Stars
+var apiUrlCovidActNow = "https://api.covidactnow.org/v2/state/CA.json?apiKey=e8eb26806f83418eade3b431bfc5fc2c";
+
+fetch(apiUrlCovidActNow)
+  .then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data);
+        //showed the # of cases
+        console.log(data.actuals.cases);
+        //shows # of deaths
+        console.log(data.actuals.deaths);
+        console.log(data.actuals.newCases);
+        console.log(data.actuals.newDeaths);
+        // console.log(data);
+      });
+    } else {
+      alert('Error: ' + response.statusText);
+    }
+  })
+  .catch(function (error) {
+    alert('Error');
+  });
 
 
 
