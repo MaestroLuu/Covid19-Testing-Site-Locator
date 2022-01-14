b7bc0d0b529fe5cedda2c557fa23749cee6f3b94
David Luu <dluu.audeo@gmail.com>
2022-01-14 03:08:42 -0800

    cleaned code for better accessibility

--- assets/js/location.js
+++ assets/js/location.js
@@ -3,7 +3,10 @@ var apiUrlLocation = "https://covid-19-testing.github.io/locations/california/co
 fetch(apiUrlLocation)
   .then(function (response) {
     if (response.ok) {
-      response.json().then(function (data) {        
+      response.json().then(function (data) {
+        // When user changes the city in the dropdown
+        // a new list of facilities corresponding to the city
+        // appends to the container below    
         $("select").on('change', function () {
           var query = (this.value);
           for (i = 0; i < data.length; i++) {
@@ -16,7 +19,8 @@ fetch(apiUrlLocation)
               var siteEl = $("<a>");
               siteEl.addClass("w3-bar-item w3-button testing-site");
               siteEl.text(facilityName);
-              $("#results-container").append(siteEl);  
+              $("#results-container").append(siteEl);
+
               // function for when facility name is clicked
               $("#results-container").on("click", function(event) {
                 for (i = 0; i < data.length; i++) {
@@ -27,13 +31,13 @@ fetch(apiUrlLocation)
                       $("#site-name").text(facilityName);
                       $("#address").text(physicalAddress.address_1 + " " + physicalAddress.city + " " 
                       + physicalAddress.state_province + " " + physicalAddress.postal_code);
+                    }
                   }
                 }
-              };
-            });
-          }
-        };
+              });
+            }
+          };
+        });
       });
-    });
-  }
+    }
   });
